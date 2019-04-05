d938bf52c2516c6c3b16932b886d3ced4c71038e
fengbao335 <xun.liu@ucdconnect.ie>
2019-04-05 18:45:30 +0100

    Update config.py

--- DublinBikes/config.py
+++ DublinBikes/config.py
@@ -1,26 +1,6 @@
-# def getConfig():
-#
-#     '''Gets all database parameters from file and returns them in dictionary format'''
-#
-#     f=open('config.config','r').read().split('\n')
-#     d={}
-#     d['database']=f[1]
-#     d['user']=f[0]
-#     d['host']=f[2]
-#     d['port']=int(f[3])
-#     d['passw']=f[4]
-#
-#     return d
-#
-# HTL
-# dbbikes
-# dbbikes.cfhvz7o7yt2w.us-east-1.rds.amazonaws.com
-# 3306
-# HTL123456
-
 import json
 config={}
-config['URI']='dbbikes.cfhvz7o7yt2w.us-east-1.rds.amazonaws.com'
+config['URI']='dbbikes.c48xjvxqywhq.us-east-1.rds.amazonaws.com'
 config['PORT']='3306'
 config['DB']='dbbikes'
 config['USER']='HTL'
