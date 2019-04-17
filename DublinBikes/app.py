37809a2674f0c1ef5b2eff2b3b90063625b7475d
ZhaolunHui <zhaolun.hui@ucdconnect.ie>
2019-04-17 18:05:03 +0100

    Add files via upload

--- DublinBikes/app.py
+++ DublinBikes/app.py
@@ -30,6 +30,7 @@ def connect_to_database():
 
 
 def get_db():
+    """把从数据库抓取的数据存到变量db里"""
     db = getattr(g, '_database', None)
     if db is None:
         db = connect_to_database()
@@ -40,6 +41,7 @@ def get_db():
 
 @app.teardown_appcontext
 def close_connection(exception):
+    """关闭和数据库的连接"""
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
@@ -47,7 +49,10 @@ def close_connection(exception):
 
 @app.route("/dynamic_stations")
 def dynamic_stations():
-    APIKEY = '5979ce80578015211c5630d5d3762548e5eba68d'
+
+    """用JCDecaux的API抓取数据，以json的形式发送到页面：dynamic_stations"""
+
+    APIKEY = '5979ce80578015211c5630d5d3762548e5eba68d'  #JCDecaux 的 API
     NAME = "Dublin"
     STATIONS_URI = "https://api.jcdecaux.com/vls/v1/stations"
     temp = requests.get(STATIONS_URI, params={"apiKey": APIKEY, "contract": NAME})
@@ -55,38 +60,19 @@ def dynamic_stations():
     return jsonify(stations=stations_detail)
 
 
-
-# @app.route("/stations")
-# def get_stations():
-#     engine = get_db()
-#     sql = "select * from STATIONS;"
-#     rows = engine.execute(sql).fetchall()
-#     print('#found {} stations', len(rows))
-#
-#     stations = jsonify(stations=[dict(row.items()) for row in rows])
-#     return stations
-
-
-# @app.route("/occupancy/<int:station_id>")
-# def get_occupancy(station_id):
-#     engine = get_db()
-#     data = []
-#     sql = "SELECT available_bikes, available_bike_stands FROM STATION where number={};".format(station_id)
-#     details = engine.execute(sql).fetchall()
-#     for row in details:
-#         data.append(dict(row))
-#     return jsonify(occupancy=data)
-
-
 @app.route("/daily/<int:station_id>")
 def week_chart(station_id):
+    """这个函数用来把参数对应的站点一周中平均每天的occupancy发送到对应的URL"""
     engine = get_db()
+    #根据传入的参数station_id，从数据库挑选出相应站点的occupancy
     sql = "SELECT available_bikes, available_bike_stands, last_update FROM STATION where number={};".format(station_id)
     rows = engine.execute(sql).fetchall()
     week_average_bikes = []
     week_average_stands = []
+    # 列表day中数值0-6代表周日到周六
     days = [0, 1, 2, 3, 4, 5, 6]
     for day in days:
+        #调用day_avg函数，计算每一天的occupancy的平均值，然后添加到列表中
         week_average_bikes.append(day_avg(rows, day)[0])
         week_average_stands.append(day_avg(rows, day)[1])
     daily = jsonify(week_average_bikes=week_average_bikes, week_average_stands=week_average_stands)
@@ -94,9 +80,11 @@ def week_chart(station_id):
 
 
 def day_avg(rows, day):
+    """这个函数用来计算每一天的occupancy的平均值"""
     available_bikes = []
     available_bike_stands = []
     for row in rows:
+        # 把unix stamp 转换成数字0-6，分别代表周日到周六
         unix_stamp = time.localtime(int(row["last_update"]))
         weekday = int(time.strftime("%w", unix_stamp))
         if weekday == day:
@@ -105,14 +93,6 @@ def day_avg(rows, day):
     day_avg_bikes = int(round((sum(available_bikes) / len(available_bikes)), 0))
     day_avg_bike_stands = int(round((sum(available_bike_stands) / len(available_bike_stands)), 0))
     return day_avg_bikes, day_avg_bike_stands
-
-
-# @app.route("/hourly/<int:station_id>")
-# def week_chart(station_id):
-#     engine = get_db()
-#     sql = "SELECT available_bikes, available_bike_stands, last_update FROM STATION where number={},;".format(station_id)
-#     rows = engine.execute(sql).fetchall()
-
 
 
 if __name__ == "__main__":
