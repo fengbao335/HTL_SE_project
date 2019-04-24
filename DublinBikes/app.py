d6d379f75e219cc1b34e67541d774c3e13babece
ZhaolunHui <zhaolun.hui@ucdconnect.ie>
2019-04-24 16:53:25 +0100

    Add files via upload

--- DublinBikes/app.py
+++ DublinBikes/app.py
@@ -3,28 +3,25 @@ from flask import Flask, render_template, g, jsonify, request
 import requests
 import json
 from sqlalchemy import create_engine
-import pymysql
 import time
 import pandas as pd
 import numpy as np
 import pickle
 
 
-# Create our flask app. Static files are served from 'static' directory
 app = Flask(__name__, static_url_path='')
 app.config.from_object('config')
 
 
-# this route simply serves 'static/index.html'
 @app.route('/')
 @app.route('/index')
 def root():
     return render_template('index.html')
 
 
-@app.route('/predict_forecast')
+@app.route('/gallery')
 def page2():
-    return render_template('predict_forecast.html')
+    return render_template('gallery.html')
 
 
 def connect_to_database():
@@ -39,7 +36,9 @@ def connect_to_database():
 
 
 def get_db():
-    """把从数据库抓取的数据存到变量db里"""
+
+    """Save the data fetched from the database to the variable：db"""
+
     db = getattr(g, '_database', None)
     if db is None:
         db = connect_to_database()
@@ -50,7 +49,9 @@ def get_db():
 
 @app.teardown_appcontext
 def close_connection(exception):
-    """关闭和数据库的连接"""
+
+    """close the connection with database"""
+
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
@@ -59,9 +60,11 @@ def close_connection(exception):
 @app.route("/dynamic_stations")
 def dynamic_stations():
 
-    """用JCDecaux的API抓取数据，以json的形式发送到页面：dynamic_stations"""
+    """
+    fetch data through JCDecaux API,Store data in json format and send to web page host:5000/dynamic_stations
+    """
 
-    APIKEY = '5979ce80578015211c5630d5d3762548e5eba68d'  #JCDecaux 的 API
+    APIKEY = '5979ce80578015211c5630d5d3762548e5eba68d'
     NAME = "Dublin"
     STATIONS_URI = "https://api.jcdecaux.com/vls/v1/stations"
     temp = requests.get(STATIONS_URI, params={"apiKey": APIKEY, "contract": NAME})
@@ -71,17 +74,24 @@ def dynamic_stations():
 
 @app.route("/daily/<int:station_id>")
 def week_chart(station_id):
-    """这个函数用来把参数对应的站点一周中平均每天的occupancy发送到对应的URL"""
+
+    """
+    This function is used to send the average daily occupancy of the site corresponding to the parameter to the corresponding URL.
+    """
+
     engine = get_db()
-    #根据传入的参数station_id，从数据库挑选出相应站点的occupancy
+    # According to the parameter:station_id
+    # select the occupancy of the corresponding station from the database.
     sql = "SELECT available_bikes, available_bike_stands, last_update FROM STATION where number={};".format(station_id)
     rows = engine.execute(sql).fetchall()
+
     week_average_bikes = []
     week_average_stands = []
-    # 列表day中数值0-6代表周日到周六
+
+    # The values 0 - 6 in the list day represent the days from Sunday to Saturday
     days = [0, 1, 2, 3, 4, 5, 6]
     for day in days:
-        #调用day_avg函数，计算每一天的occupancy的平均值，然后添加到列表中
+        # Invoking the function:day_avg, calculate the average occupancy on a single day, and then add it to the list
         week_average_bikes.append(day_avg(rows, day)[0])
         week_average_stands.append(day_avg(rows, day)[1])
     daily = jsonify(week_average_bikes=week_average_bikes, week_average_stands=week_average_stands)
@@ -89,25 +99,37 @@ def week_chart(station_id):
 
 
 def day_avg(rows, day):
-    """这个函数用来计算每一天的occupancy的平均值"""
+
+    """This function calculates the average of occupancy on a single day"""
+
     available_bikes = []
     available_bike_stands = []
+
     for row in rows:
-        # 把unix stamp 转换成数字0-6，分别代表周日到周六
+        # Convert Unix stamp to Numbers 0-6, representing Sunday to Saturday
         unix_stamp = time.localtime(int(row["last_update"]))
         weekday = int(time.strftime("%w", unix_stamp))
         if weekday == day:
             available_bikes.append(row["available_bikes"])
             available_bike_stands.append(row["available_bike_stands"])
+
     day_avg_bikes = int(round((sum(available_bikes) / len(available_bikes)), 0))
     day_avg_bike_stands = int(round((sum(available_bike_stands) / len(available_bike_stands)), 0))
+
     return day_avg_bikes, day_avg_bike_stands
 
 
 @app.route("/bikes_prediction/<int:station_id>/<int:time_hour>")
 def bikes_prediction(station_id,time_hour):
+
+    """
+    This function predicts the occupancy of selected station, depending on the model we trained
+    """
+
+    # get the data through openWeather api
     r = requests.get('http://api.openweathermap.org/data/2.5/forecast?appid=9511c6f09bf671d3bd65bf650197234f&q=Dublin')
     weathers = r.json()
+
     weather_detalis = weathers["list"]
     temp = weather_detalis[0]['main']['temp']
     wind = weather_detalis[0]['wind']['speed']
@@ -118,16 +140,16 @@ def bikes_prediction(station_id,time_hour):
         weather_Drizzle = 1
     elif main == 'Rain':
         weather_Rain = 1
-    f2 = pd.DataFrame(np.array([station_id,time_hour, temp, wind, weather_Drizzle, weather_Rain])).T
+    f2 = pd.DataFrame(np.array([station_id, time_hour, temp, wind, weather_Drizzle, weather_Rain])).T
     models = {}
+    # open the folder of model
     with open('static/pickle/'+str(station_id) + ".pkl", "rb") as handle:
         models[station_id] = pickle.load(handle)
     available_bikes_prediction = models[station_id].predict(f2).round()[0]
     return jsonify(bp=available_bikes_prediction)
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=5001)
 
 
 
